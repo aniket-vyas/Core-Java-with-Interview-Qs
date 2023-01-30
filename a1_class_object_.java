6a65837475cedb5af1ffa7ddfa00aa0dd5b51a14
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-01-30 23:44:11 +0530

    Create a1_class_object_.java

---
+++ a1_class_object_.java
@@ -0,0 +1,18 @@
+/*
+	 *    CLASS :-
+	 * ~> template for object
+	 * ~> blueprint for creating object
+	 * ~> it helps us to generate object using "new" keyword
+	 * 
+	 *    OBJECT :-
+	 * ~> instance of a class
+	 * ~> it is a real world entity that has an identity, behavior, state. 
+*/
+public class class_object {
+	public static void main(String[] args) {
+		class_object a1 = new class_object();
+		System.out.println(a1);
+		
+		// it will print add of reference variable or object add
+	}
+}
