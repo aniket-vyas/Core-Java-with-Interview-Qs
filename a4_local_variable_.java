61a4128dd15011818824b8fb4bd31cbf87031ba1
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-01 23:14:05 +0530

    Create a4_local_variable_.java

---
+++ a4_local_variable_.java
@@ -0,0 +1,20 @@
+/*
+ * 	3.) LOCAL VARIABLE :-
+ * 
+ * ~> it should be used only with in created method
+ * ~> if we use outside created method we get an error
+ */
+
+public class local_variable {
+	public static void main(String[] args) {
+		int x = 10;		//local variable	
+		System.out.println(x); // O/P = 10
+	}
+	
+//	we use outside created method
+	
+//	public void test() {	
+//		System.out.println(x);  //error
+//	}
+	
+}
