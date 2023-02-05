07e6a177d18becb5cb0e5f6be2c8006de3041102
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-05 13:19:46 +0530

    Create a10_IIB_.java

---
+++ a10_IIB_.java
@@ -0,0 +1,40 @@
+/*
+ *		IIB :- Instance Initialization Block
+ *
+ * ~> it will execute only when object is created.
+ * ~> no. of times object is created, same no. of 
+ * 		times IIB will be called. 
+ * ~> it is use to initialized all the instance variable.
+ * ~> first IIB will call then constructor
+ * ~> we can initialized both static and non-static
+ * 		variable inside IIB.
+*/
+public class IIB {
+	
+	//IIB-1
+	{
+		System.out.println("From IIB-1");
+	}
+	
+	//constructor
+	IIB(){
+		System.out.println("From constructor");
+	}
+	
+	//IIB-2
+	int i;		//static int i; 
+	{
+		System.out.println("From IIB-2");
+		i = 20;
+		System.out.println("From IIB-2 , i = " + i);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Start main");
+		
+		IIB a = new IIB(); //call IIB then constructor
+		IIB b = new IIB(); //call IIB then constructor
+		
+		System.out.println("End main");
+	}
+}
