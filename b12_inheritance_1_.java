9207a6c303734ec80b68c53afd6ba905de43a825
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-07 23:41:34 +0530

    Create b12_inheritance_1_.java

---
+++ b12_inheritance_1_.java
@@ -0,0 +1,13 @@
+/*
+ *		INHERITANCE :-
+ * 
+ *  ~> "extends" keyword is use to perform inheritance
+ *  ~> whatever present in class(inheritance_1) we will
+ *  	inherit to class(inheritance_2).
+*/
+public class inheritance_1 {
+	int x = 10;   //non-static variable
+	public void test() {    //non-static()
+		System.out.println(100);
+	}
+}
