022ee60686d4778084a1f76158d4e0baa9451880
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-08 19:52:39 +0530

    Create b14_package_1_.java

---
+++ b14_package_1_.java
@@ -0,0 +1,16 @@
+/*
+ * 		PACKAGES :-
+ * 
+ *  ~> it is nothing but folder creating in Java,
+ *  	to store programs in organized manner
+ *  ~> how to create a package :
+ *  	-> "src" right click > new > package
+ *  ~> to access a class present in different packages
+ *  	we have to import the class
+*/
+package test; //package name : test
+public class pack_1 {
+	
+	//empty
+	
+}
