f1c284a9bb6d590a0a9437ab5e774e17c7766ddd
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-08 19:55:04 +0530

    Create b15_package_2_.java

---
+++ b15_package_2_.java
@@ -0,0 +1,10 @@
+package test2; //package name : test2
+
+//we import the class(pack_1) from package(test)
+import test.pack_1;	
+public class pack_2{
+	public static void main(String[] args) {
+		pack_1 a1 = new pack_1();
+		System.out.println(a1);
+	}	
+}
