4f0dc1c20913d064926779e62c0031b08edc811d
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-09 20:34:07 +0530

    Create b16_private_.java

---
+++ b16_private_.java
@@ -0,0 +1,34 @@
+/*
+ * 		ACCESS SPECIFIER / MODIFIER
+ * 
+ * ~> 4 types are :-
+ * 		1.) private
+ * 		2.) default
+ * 		3.) protected
+ * 		4.) public
+ * 
+ * 1.) private :-
+ * 		-methods & variables :
+ * 			it will not work outside class
+ * 		-constructor :
+ * 			if you make constructor private
+ * 			then it's object can be created 
+ * 			same class only
+ * 		-classes :
+ * 			a class can never be private
+*/
+public class private_ {
+	private int x = 10; //variable
+	private void test() { //method
+		System.out.println("from test()");
+	}
+	private private_(){ //constructor
+		System.out.println("from constructor");
+	}
+	public static void main(String[] args) {
+		//calling
+		private_ a1 = new private_();
+		System.out.println(a1.x);
+		a1.test();
+	}
+}
