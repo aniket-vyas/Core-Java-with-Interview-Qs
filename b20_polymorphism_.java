989287f37ad01a60b37a241d47612ea08d7516fc
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-12 23:36:03 +0530

    Create b20_polymorphism_.java

---
+++ b20_polymorphism_.java
@@ -0,0 +1,16 @@
+/*
+ *		POLYMORPHISM :-
+ *
+ * ~> whenever 1 entity can take multiple forms
+ * 		depending on the situations
+ * ~> it is applicable only on methods
+ * 		not variables
+ * ~>types :
+ * 		1.) Overriding
+ * 		2.) Overloading
+*/
+public class polymorphism {
+	public void test() {
+		System.out.println(100);
+	}
+}
