fdf5f7981b02ce59e262d68c84276ef85d185dde
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-13 23:11:07 +0530

    Create b23_interface_1_.java

---
+++ b23_interface_1_.java
@@ -0,0 +1,19 @@
+/*
+ *			INTERFACE :-
+ *
+ * ~> it can consist of only incompelete method
+ * 		otherwise it will show error
+ * ~> we can do inheritance from :
+ * 		class to class --> using extends
+ * 		interface to interface --> using extends
+ * 		interface to class --> using implements
+ * 		class to interface --> not possible
+ * ~> interface can support multiple interface
+ * ~> marker interface : an empty interface
+*/
+public interface interface_1 {
+	
+	//incompelete methods
+	public void test_1();
+	public void test_2();
+}
