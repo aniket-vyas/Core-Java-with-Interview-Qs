dbedb99c79fc736f5d431a820c171a82277441e3
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-15 19:35:33 +0530

    Create b26_functional_interface_1_.java

---
+++ b26_functional_interface_1_.java
@@ -0,0 +1,16 @@
+/*
+ *		FUNCTIONAL INTERFACE :-
+ *
+ * ~> it is introduced in Java 8
+ * ~> you should create exactly one incompelete method
+ * ~> functional interface will not support 
+ * 		multiple inheritance
+*/
+
+@FunctionalInterface
+public interface functional_interface_1 {
+	
+	//incompelete method
+	public void test();
+  
+}
