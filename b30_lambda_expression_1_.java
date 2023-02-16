3fbd034775598e438c2fc26a1c126cdb2788f3e4
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-02-16 21:27:53 +0530

    Create b30_lambda_expression_1_.java

---
+++ b30_lambda_expression_1_.java
@@ -0,0 +1,17 @@
+/*
+ * 		LAMBDA EXPRESSION :-
+ *
+ * ~> we can reduce the number of lines of code
+ * ~> there is no need of creating object
+ * ~> you can use that in certain place
+ * 		like -> functional interface
+ * ~> object creation are not required to access 
+ * 		non-static members
+ * ~> that's why we call as functional programming
+ * 		language (since Java 8)
+*/
+
+@FunctionalInterface
+public interface lambda_expression_1 {
+	public void test();
+}
