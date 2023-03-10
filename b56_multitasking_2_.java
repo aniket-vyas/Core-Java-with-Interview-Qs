a45c749cc00b94c719558ce2e00b07cfa08f2df3
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-03-10 23:08:08 +0530

    Create

---
+++ b56_multitasking_2_.java
@@ -0,0 +1,36 @@
+/*
+ *		RUNNABLE :- 
+*/
+public class multitasking_2 implements Runnable{
+
+	String name;
+	multitasking_2(String name){
+		this.name = name;
+	}
+	
+	@Override
+	public void run() {
+		for(int i = 0 ; i<10000 ; i++) {
+			System.out.println(this.name);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		//Thread
+		multitasking_2 a1 = new multitasking_2("XXXX");
+		Thread t1 = new Thread(a1);
+		t1.start();
+		
+		//Thread
+		multitasking_2 a2 = new multitasking_2("YYYY");
+		Thread t2 = new Thread(a2);
+		t2.start();
+		
+		for(int i = 0 ; i<10000 ; i++) {
+			System.out.println("main");
+		}
+	}
+	
+	//OUTPUT :- YYYY .... main .... XXXX .... (not fix)
+}
