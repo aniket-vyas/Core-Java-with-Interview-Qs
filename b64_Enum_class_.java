51add052332683e4087bb70e30e7ea2221cdc9ee
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-03-23 23:08:20 +0530

    Create b64_Enum_class_.java

---
+++ b64_Enum_class_.java
@@ -0,0 +1,10 @@
+/*
+ *		ENUM :-
+ *
+ * ~> it is a special class , that represents a group of constants
+ * ~> how to create
+ * 		src > new > Enum
+*/
+public enum Calendar {
+	Jan , Feb , Mar , Apr , May , Jun , Juy , Aug , Sep , Oct , Nov , Dec
+}
