948c175163a293c8b81ca1982dd9d8369a0fdab5
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-03-27 23:30:51 +0530

    Create b69_throw_keyword_1_.java

---
+++ b69_throw_keyword_1_.java
@@ -0,0 +1,13 @@
+/*
+ *		"throw" KEYWORD :-
+ *
+ * ~> it help us to create customize exception as per the requirement of the developer
+ * ~> how to create ?
+ * 		-> create a class , which is subclass of Throwable
+*/
+
+public class InSufficientFunds extends Throwable{
+	
+	//empty
+
+}
