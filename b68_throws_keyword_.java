c70f0037c28b8afd2ab94660dfb1a56168a05b7c
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-03-28 22:48:39 +0530

    Update b68_throws_keyword_.java

--- b68_throws_keyword_.java
+++ b68_throws_keyword_.java
@@ -4,16 +4,25 @@
  * ~> it is applied on a method
  * ~> if any exception occur in a method then the exception will be passed on the
  * 		calling statement of a method
+ * ~> after "throws" keyword you can give multiple class names
 */
 
 import java.io.FileWriter;
 import java.io.IOException;
-public class A {
-	public static void main(String[] args) throws IOException {
-		A a1 = new A();
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class throws_ {
+	
+	public static void main(String[] args) throws IOException, SQLException {
+		throws_ a1 = new throws_();
 		a1.test();
 	}
-	public void test() throws IOException {
+	
+	public void test() throws IOException, SQLException {
+		
+		//Don't worry of this code & enjoy learning
 		FileWriter fw = new FileWriter("D://test.txt");
+		DriverManager.getConnection(" "," "," ");
+		
 	}
 }
