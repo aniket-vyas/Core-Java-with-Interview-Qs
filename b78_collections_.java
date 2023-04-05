bf5a7e6b67f0d445479ba87ad7eee656227a81a0
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-04-05 22:26:36 +0530

    Create b78_collections_.java

---
+++ b78_collections_.java
@@ -0,0 +1,41 @@
+/*
+ *		COLLECTIONS IN JAVA :- 
+ *
+ * ~> it help us to store group of object in it.
+ * ~> collection is a framework which have readly available logic in it that can deal
+ * 		with the data structure in easy way
+ * ~> java.util package contains all the classes & interface for the collection
+ * 		framework
+ * 
+ * 						Iterable -> root interface for all collection class
+ * 							|
+ * 						Collection -> interface
+ * 							|
+ * 				|-----------|-----------|
+ * 				|			|			|
+ * 				List		Queue		Set -----------------
+ * 				|			|			|					|
+ * 				|			|			|					|
+ * 			~ArrayList	~PriorityQueue	~HashSet			~SortedSet(Interface)
+ * 			~LinkedList	~Deque			~LinkedHashSet		 -> TreeSet(Class)
+ * 			~Vector		 -> ArrayDeque	
+ * 			 -> Stack
+ * 
+ * 
+ * 		Map -> (interface)not inside collection
+ * 		|
+ * 		|--> HashMap (class)
+ * 		|
+ * 		|--> Dictionary Class
+ * 			 -> Hash Table
+*/
+public class A {
+	public static void main(String[] args) {
+		/*
+		 *		List , Queue , Set --> Interface
+		 *
+		 * ArrayList  , LinkedList , Vector --> Class
+		 * HashSet , LinkedHashSet --> Class
+		 */
+	}
+}
