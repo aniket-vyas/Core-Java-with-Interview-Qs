b052d0d13d5bf3fe12f0045ce6234b4a2ca9bcba
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-04-17 14:53:21 +0530

    Create b93_Encapsulation_2_.java

---
+++ b93_Encapsulation_2_.java
@@ -0,0 +1,12 @@
+public class Encapsulation_2 {
+	public static void main(String[] args) {
+		
+		Encapsulation_1 a1 = new Encapsulation_1();
+		
+		a1.setId(100);
+		
+		int val = a1.getId();
+		
+		System.out.println(val);
+	}
+}
